48141bb548dedbe2a347ed455b11319bcc21f7bb
Felipe Eduardo Duzzi <duzzifelipe@gmail.com>
2020-09-09 17:32:09 -0300

    chore: run migrations

---
+++ lib/my_app/commands/release_tasks.ex
@@ -0,0 +1,13 @@
+defmodule MyApp.Command.ReleaseTasks do
+  @moduledoc false
+
+  def migrate do
+    {:ok, _} = Application.ensure_all_started(:my_app)
+
+    path = Application.app_dir(:my_app, "priv/repo/migrations")
+
+    Ecto.Migrator.run(MyApp.Repo, path, :up, all: true)
+
+    :init.stop()
+  end
+end
